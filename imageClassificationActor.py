92ad49ee15a1bafd3676f905236f4058dbcd8a17
mquinaz <36314718+mquinaz@users.noreply.github.com>
2021-04-21 02:46:12 +0100

    Add files via upload
    Model h5 support

--- imageClassificationActor.py
+++ imageClassificationActor.py
@@ -32,6 +32,7 @@ class ImageClassificationActor(DynamicActor):
 		self.mode = self.MODE_NOT_CONFIGURED
 		self.vc = None
 		self.model = None
+		self.model_type = 0
 		self.sample_freq = 0
 		self.frame_counter = 0
 		self.cam = None
@@ -42,26 +43,44 @@ class ImageClassificationActor(DynamicActor):
 		self.labels = None
         
 	def create_model(self,file_model):
-		self.interpreter = tf.lite.Interpreter(model_path= (self.PATH_DIR + file_model) )
-		self.interpreter.allocate_tensors()
+		if(file_model.split('.')[1] == 'tflite'):
+			self.interpreter = tf.lite.Interpreter(model_path= (self.PATH_DIR + file_model) )
+			self.interpreter.allocate_tensors()
 
-		self.input_details = self.interpreter.get_input_details()
-		self.output_details = self.interpreter.get_output_details()
-		return self.interpreter
+			self.input_details = self.interpreter.get_input_details()
+			self.output_details = self.interpreter.get_output_details()
+			self.model_type = 0
+			return self.interpreter
+		if(file_model.split('.')[1] == 'h5'):
+			self.model = load_model(file_model)
+			self.model_type = 1
+			return self.model
 	
 	def classify_Image(self,file_name,top_k=1):
-		input_shape = self.input_details[0]['shape']
-		input_data = np.expand_dims(cv2.resize( cv2	.imread(file_name) , (64,64) ) , axis=0).astype(np.float32)
-		input_data = (np.float32(input_data) - 127.5) / 127.5
-		self.interpreter.set_tensor(self.input_details[0]['index'], input_data)
+		if(self.model_type == 0):
+			input_shape = self.input_details[0]['shape']
+			input_data = np.expand_dims(cv2.resize( cv2	.imread(file_name) , (64,64) ) , axis=0).astype(np.float32)
+			input_data = (np.float32(input_data) - 127.5) / 127.5
+			self.interpreter.set_tensor(self.input_details[0]['index'], input_data)
 
-		self.interpreter.invoke()
-		output_data = self.interpreter.get_tensor(self.output_details[0]['index'])
-		results = np.squeeze(output_data)
-		results = tf.nn.softmax(results).numpy()
+			self.interpreter.invoke()
+			output_data = self.interpreter.get_tensor(self.output_details[0]['index'])
+			results = np.squeeze(output_data)
+			results = tf.nn.softmax(results).numpy()
 
-		ordered = np.argpartition(-results, top_k)
-		return [(i, results[i]) for i in ordered]
+			ordered = np.argpartition(-results, top_k)
+			return [(i, results[i]) for i in ordered]
+
+		if (self.model_type == 1):
+			img = Image.open(img_file)
+			img = img.convert('RGB')
+			img_array = np.asarray(img) / 255
+			img_array = tf.expand_dims(img_array, 0)
+
+			predictions = model.predict(img_array, verbose=True)
+			score = tf.nn.softmax(predictions[0])
+			print(np.argmax(score) + " - " +  (100 * np.max(score)) )
+			return np.argmax(score), 100 * np.max(score)
 		
 	def from_target(self, msg):
 		try:
@@ -142,7 +161,7 @@ class ImageClassificationActor(DynamicActor):
 
 		for tempTuple in results:
 			label_id, prob = tempTuple
-			print("%s - %.2f" % (self.labels[label_id], prob ) )
+			print("%s - %.2f - " % (self.labels[label_id], prob ) , end='')
 
 			new_Classification = pyimc.ScoredClassification()
 			prob = abs(prob)
@@ -150,6 +169,7 @@ class ImageClassificationActor(DynamicActor):
 			new_Classification.classification = self.labels[label_id]
 			msg.classifications.append(new_Classification)
 
+		print("")
 		msg.frameid = self.frame_counter
 		#msg.data = frame.tobytes()
 		msg.data = cv2.imencode('.png',frame)[1].tobytes()
