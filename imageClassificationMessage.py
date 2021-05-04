6d7350a52654776489a78a6d027f8704d4a3afa4
mquinaz <36314718+mquinaz@users.noreply.github.com>
2021-05-04 20:40:41 +0100

    Add files via upload
    Changed changes to send a IMC message (still not correct)

--- imageClassificationMessage.py
+++ imageClassificationMessage.py
@@ -1,36 +1,35 @@
 import sys
+
+import pyimc
 from pyimc.network.udp import IMCSenderUDP
+import socket, struct, asyncio, logging
+from pyimc.common import multicast_ip
 
 class ImageClassificationMessage:
 
-	def __init__(self,imc_id):
+	def __init__(self,imc_id,host,port):
 		self.imc_id = imc_id
-		self.host = ''  # Symbolic name meaning the local host
-		self.port = 0  #	 Arbitrary non-privileged port
-		self.command = 0
-		self.model = None
-		self.video_source = None
-		self.sampling_freq = 0
+		self.host = host
+		self.port = port
 
-	def send_message(self):
-		if message == None :
+	def send_message(self,msg):
+		if msg == None :
 			print("ERROR: unknown message: " + sys.argv[3])
 			return
 
-		# change "test" to IMC message
 		socket = IMCSenderUDP(self.host,self.port)
-		socket.send("test",self.port)
+		#socket.__enter__()
+		socket.send(msg,self.port)
 
 if __name__ == '__main__':
-	message = ImageClassificationMessage(1234)
+	control = ImageClassificationMessage('1234', sys.argv[1], sys.argv[2])
 	print(sys.argv)
 	#./dune-sendmsg localhost 6001 ImageClassificationControl 2 newModel.tflite /home/miguel/Downloads/example.mjpg 0.2
 	if sys.argv[3] ==  "ImageClassificationControl" :
-		message.host = sys.argv[1]
-		message.port = sys.argv[2]
-		message.command = sys.argv[4]
+		message = pyimc.ImageClassificationControl()
+		message.command = int(sys.argv[4])
 		message.model = sys.argv[5]
 		message.video_source = sys.argv[6]
-		message.sampling_freq = sys.argv[7]
+		message.sampling_freq = float(sys.argv[7])
 
-		message.send_message()
+		control.send_message(message)
