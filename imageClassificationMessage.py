55936b6c61bdb2926d529b25d4c52aaa8085380d
mquinaz <36314718+mquinaz@users.noreply.github.com>
2021-05-07 11:53:20 +0100

    Add files via upload
    Meeting changes to UDP connection

--- imageClassificationMessage.py
+++ imageClassificationMessage.py
@@ -5,24 +5,7 @@ from pyimc.network.udp import IMCSenderUDP
 import socket, struct, asyncio, logging
 from pyimc.common import multicast_ip
 
-class ImageClassificationMessage:
-
-	def __init__(self,imc_id,host,port):
-		self.imc_id = imc_id
-		self.host = host
-		self.port = port
-
-	def send_message(self,msg):
-		if msg == None :
-			print("ERROR: unknown message: " + sys.argv[3])
-			return
-
-		socket = IMCSenderUDP(self.host,self.port)
-		#socket.__enter__()
-		socket.send(msg,self.port)
-
 if __name__ == '__main__':
-	control = ImageClassificationMessage('1234', sys.argv[1], sys.argv[2])
 	print(sys.argv)
 	#./dune-sendmsg localhost 6001 ImageClassificationControl 2 newModel.tflite /home/miguel/Downloads/example.mjpg 0.2
 	if sys.argv[3] ==  "ImageClassificationControl" :
@@ -32,4 +15,6 @@ if __name__ == '__main__':
 		message.video_source = sys.argv[6]
 		message.sampling_freq = float(sys.argv[7])
 
-		control.send_message(message)
+
+		with IMCSenderUDP(sys.argv[1], None) as socket:
+			socket.send(message, int(sys.argv[2]) )
