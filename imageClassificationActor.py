6aa7a1483971c633429823dca8161ccf5747160b
mquinaz <36314718+mquinaz@users.noreply.github.com>
2021-05-11 19:02:38 +0100

    Add files via upload
    Added initial refactoring

--- imageClassificationActor.py
+++ imageClassificationActor.py
@@ -18,6 +18,7 @@ from PIL import Image
 import numpy as np
 
 import tfmodel
+import h5model
 
 class ImageClassificationActor(DynamicActor):
 	MODE_NOT_CONFIGURED = 0
@@ -34,45 +35,16 @@ class ImageClassificationActor(DynamicActor):
 		self.heartbeat.append(target_name)
 		self.mode = self.MODE_NOT_CONFIGURED
 		self.vc = None
-		self.model = None
-		self.model_type = 0
+		self.model_type = -1
 		self.sample_freq = 0
 		self.frame_counter = 0
 		self.cam = None
-		self.interpreter = None
-		self.input_details = None
-		self.output_details = None
 		self.last_run = time.time()
 		self.labels = None
-		self.model_type = -1
-	
-	def classify_Image(self,file_name,top_k=1):
-		if(self.model_type == 0):
-			input_shape = self.input_details[0]['shape']
-			input_data = np.expand_dims(cv2.resize( cv2	.imread(file_name) , (64,64) ) , axis=0).astype(np.float32)
-			input_data = (np.float32(input_data) - 127.5) / 127.5
-			self.interpreter.set_tensor(self.input_details[0]['index'], input_data)
 
-			self.interpreter.invoke()
-			output_data = self.interpreter.get_tensor(self.output_details[0]['index'])
-			results = np.squeeze(output_data)
-			#results = tf.nn.softmax(results).numpy()
+		self.aux = None
+		self.list_model_param = None
 
-			print(results)
-			ordered = np.argpartition(-results, top_k)
-			return [(i, results[i]) for i in ordered]
-
-		if (self.model_type == 1):
-			img = Image.open(file_name)
-			img = img.convert('RGB')
-			img_array = np.asarray(img) / 255
-			img_array = tf.expand_dims(img_array, 0)
-
-			predictions = self.model.predict(img_array, verbose=True)
-			score = tf.nn.softmax(predictions[0])
-			print(np.argmax(score) + " - " +  (100 * np.max(score)) )
-			return np.argmax(score), 100 * np.max(score)
-		
 	def from_target(self, msg):
 		try:
 			node = self.resolve_node_id(msg)
@@ -85,18 +57,9 @@ class ImageClassificationActor(DynamicActor):
 	def on_Classification_Control(self, msg: pyimc.ImageClassificationControl):
 		logger.info('Received')
 		if msg.command == pyimc.ImageClassificationControl.CommandEnum.SETUP:
-			aux = tfmodel.tfmodel(self.PATH_DIR,msg.model)
-			list_model_param = aux.create_model()
-			if len(list_model_param) == 5:
-				self.interpreter = list_model_param[0]
-				self.input_details = list_model_param[1]
-				self.output_details = list_model_param[2]
-				self.model_type = list_model_param[3]
-				self.labels = list_model_param[4]
-			if len(list_model_param) == 3:
-				self.model = list_model_param[0]
-				self.model_type = list_model_param[1]
-				self.labels = list_model_param[2]
+			self.aux = tfmodel.tfmodel(self.PATH_DIR,msg.model)
+			self.list_model_param = self.aux.create_model()
+			self.labels = self.list_model_param[-1]
 
 			self.mode = self.MODE_CONFIGURED
 			self.sample_freq = msg.sampling_freq
@@ -153,7 +116,7 @@ class ImageClassificationActor(DynamicActor):
 		logger.info("{} written!".format(img_name))
 
 		start_time = time.time()
-		results = self.classify_Image(img_name)
+		results = self.aux.classify_Image(img_name,self.list_model_param)
 		elapsed_ms = (time.time() - start_time) * 1000
 		print("Elapsed time: %.2f" % (elapsed_ms))
 		msg = pyimc.ImageClassification()
