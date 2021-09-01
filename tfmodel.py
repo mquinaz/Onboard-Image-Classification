fix scaling in FP models

tfmodel.py
@@ -26,9 +26,10 @@ class Classifier:
     self.width = self.input_details[0]['shape'][2]
 
   def classify(self,frame,top_k=1):
-    input_data = np.expand_dims(cv2.resize(frame, (self.width, self.height)), axis=0)#.astype(np.float32)
+    input_data = np.expand_dims(cv2.resize(frame, (self.width, self.height)), axis=0)
+    print(input_data.shape)
     if self.floating_model:
-      input_data = (np.float32(input_data) - 127.5) / 127.5
+      input_data = np.float32(input_data) / 255.0
     self.interpreter.set_tensor(self.input_details[0]['index'], input_data)
     self.interpreter.invoke()
     output_data = self.interpreter.get_tensor(self.output_details[0]['index'])